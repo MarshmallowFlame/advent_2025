46ba28daedd5b035602923429ac9e28cfd0c5fe7
Svante Rollenhagen <svante.rollenhagen@gmail.com>
2025-12-03 22:20:36 +0100

    add comments on day 1 part 2 solution

--- 01/02.hs
+++ 01/02.hs
@@ -22,8 +22,27 @@ applyTurn (position, nrZeroes) nextTurn =
             newPosition = mod newPositionRaw 100
 
             -- There are a number of ways you can land on or pass 0.
+            -- I see it like there is a symmetry around 0, and each
+            -- multiple of 100 (on either side of absolute 0) corresponds
+            -- to 0 mod 100.
+            -- You always start in the first positive interval between 0 and 100,
+            -- and so there are a number of zeroes you can cross or land on.
+            -- I divide that into the "true" zero, and "fake" zeroes.
+
+            -- This first check counts how many times we are passing or landing on
+            -- these "other" zeroes, by dividing the new position with 100 mod 100.
+            -- This will only account for crossing or landing on "fake" zeroes e.g.
+            -- 100 or -200.
             zeroesFromWholeTurns = quot (abs newPositionRaw) 100
+
+            -- The next check is for whether we crossed from positives into negatives,
+            -- i.e. crossing the "true" zero. We do not want to count if we started on
+            -- the "true" zero and continue into negatives, as that will be already have
+            --been accounted for in the previous turn.
             flipped = if ((signum newPositionRaw == -1) && (position/=0)) then 1 else 0
+
+            -- Finally we want to check if we landed on "true" zero. Landing on any
+            -- other zeroes will be accounted for by zeroesFromWholeTurns.
             isZero = if newPositionRaw == 0 then 1 else 0
             newZeroes = nrZeroes+zeroesFromWholeTurns+flipped+isZero
         in (newPosition, newZeroes)
