68843f6f4a693a751f6a27c4f44470b8ddc13d22
Svante Rollenhagen <svante.rollenhagen@gmail.com>
2025-12-04 22:29:53 +0100

    Formatting

--- 01/01.hs
+++ 01/01.hs
@@ -1,20 +1,19 @@
-main = do  
-    contents <- readFile "input.txt"
-    let theWords = lines contents
-    let (finalPosition, finalNrZeroes) = foldl applyTurn (50,0) theWords
-    print finalNrZeroes
+main = do
+  contents <- readFile "input.txt"
+  let theWords = lines contents
+  let (finalPosition, finalNrZeroes) = foldl applyTurn (50, 0) theWords
+  print finalNrZeroes
 
 getDirectedStep :: String -> Integer
 getDirectedStep a = case a of
-        (d:steps) -> case d of
-                'R' -> 1 * read steps
-                'L' -> -1 * read steps
+  (d : steps) -> case d of
+    'R' -> read steps
+    'L' -> -read steps
 
 -- Input is the current state (position, nrZeroes) and next turn as a string
 applyTurn :: (Integer, Integer) -> String -> (Integer, Integer)
 applyTurn (position, nrZeroes) nextTurn =
-        let directedStep = getDirectedStep nextTurn
-            newPosition = mod (position + directedStep) 100
-            isZero = if newPosition == 0 then 1 else 0
-        in (newPosition, nrZeroes+isZero)
-         
+  let directedStep = getDirectedStep nextTurn
+      newPosition = mod (position + directedStep) 100
+      isZero = if newPosition == 0 then 1 else 0
+   in (newPosition, nrZeroes + isZero)
--- 01/02.hs
+++ 01/02.hs
@@ -1,45 +1,45 @@
-main = do  
-    contents <- readFile "input.txt"
-    let theWords = lines contents
-    let (finalPosition, finalNrZeroes) = foldl applyTurn (50,0) theWords
-    print finalNrZeroes
+main = do
+  contents <- readFile "input.txt"
+  let theWords = lines contents
+  let (finalPosition, finalNrZeroes) = foldl applyTurn (50, 0) theWords
+  print finalNrZeroes
 
 getDirectedStep :: String -> Int
 getDirectedStep a = case a of
-        (d:steps) -> case d of
-                'R' -> 1 * read steps
-                'L' -> -1 * read steps
+  (d : steps) -> case d of
+    'R' -> 1 * read steps
+    'L' -> -1 * read steps
 
 -- Input is the current state (position, nrZeroes) and next turn as a string
 applyTurn :: (Int, Int) -> String -> (Int, Int)
 applyTurn (position, nrZeroes) nextTurn =
-        let directedStep = getDirectedStep nextTurn
+  let directedStep = getDirectedStep nextTurn
 
-            newPositionRaw = (position + directedStep)
-            newPosition = newPositionRaw `mod` 100
+      newPositionRaw = (position + directedStep)
+      newPosition = newPositionRaw `mod` 100
 
-            -- There are a number of ways you can land on or pass 0.
-            -- I see it like there is a symmetry around 0, and each
-            -- multiple of 100 (on either side of absolute 0) corresponds
-            -- to 0 mod 100.
-            -- You always start in the first positive interval between 0 and 100,
-            -- and so there are a number of zeroes you can cross or land on.
-            -- I divide that into the "true" zero, and "fake" zeroes.
+      -- There are a number of ways you can land on or pass 0.
+      -- I see it like there is a symmetry around 0, and each
+      -- multiple of 100 (on either side of absolute 0) corresponds
+      -- to 0 mod 100.
+      -- You always start in the first positive interval between 0 and 100,
+      -- and so there are a number of zeroes you can cross or land on.
+      -- I divide that into the "true" zero, and "fake" zeroes.
 
-            -- This first check counts how many times we are passing or landing on
-            -- these "other" zeroes, by dividing the new position with 100 mod 100.
-            -- This will only account for crossing or landing on "fake" zeroes e.g.
-            -- 100 or -200.
-            zeroesFromWholeTurns = abs newPositionRaw `quot` 100
+      -- This first check counts how many times we are passing or landing on
+      -- these "other" zeroes, by dividing the new position with 100 mod 100.
+      -- This will only account for crossing or landing on "fake" zeroes e.g.
+      -- 100 or -200.
+      zeroesFromWholeTurns = abs newPositionRaw `quot` 100
 
-            -- The next check is for whether we crossed from positives into negatives,
-            -- i.e. crossing the "true" zero. We do not want to count if we started on
-            -- the "true" zero and continue into negatives, as that will be already have
-            --been accounted for in the previous turn.
-            flipped = fromEnum $ signum newPositionRaw == -1 && position/=0
+      -- The next check is for whether we crossed from positives into negatives,
+      -- i.e. crossing the "true" zero. We do not want to count if we started on
+      -- the "true" zero and continue into negatives, as that will be already have
+      -- been accounted for in the previous turn.
+      flipped = fromEnum $ signum newPositionRaw == -1 && position /= 0
 
-            -- Finally we want to check if we landed on "true" zero. Landing on any
-            -- other zeroes will be accounted for by zeroesFromWholeTurns.
-            isZero = fromEnum $ newPositionRaw == 0
-            newZeroes = nrZeroes+zeroesFromWholeTurns+flipped+isZero
-        in (newPosition, newZeroes)
+      -- Finally we want to check if we landed on "true" zero. Landing on any
+      -- other zeroes will be accounted for by zeroesFromWholeTurns.
+      isZero = fromEnum $ newPositionRaw == 0
+      newZeroes = nrZeroes + zeroesFromWholeTurns + flipped + isZero
+   in (newPosition, newZeroes)
