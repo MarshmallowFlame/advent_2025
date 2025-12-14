ed394396ce1541c114564764b2d09ad8ac07103b
Svante Rollenhagen <svante.rollenhagen@gmail.com>
2025-12-14 20:49:31 +0100

    Fixes to day 4 part 1
    probably claudes tips

--- 04/01.hs
+++ 04/01.hs
@@ -6,18 +6,17 @@ main = do
   let l = lines contents
       hpadded = replicate (length $ head l) '.' : l ++ [replicate (length $ head l) '.']
       padded = map (\s -> '.' : s ++ ['.']) hpadded
-      zipped = zip3 padded (tail padded) (tail (tail padded))
+      zipped = zipS padded
       deepZipped = map (applyTriple zipS) zipped
       conv2d = concatMap zipThrees deepZipped
-      accs = map accessable conv2d
-      count = sum $ map fromEnum accs
+      count = length $ filter accessable conv2d
 
   print count
 
 applyTriple :: (a -> b) -> (a, a, a) -> (b, b, b)
 applyTriple f (a, b, c) = (f a, f b, f c)
 
-zipS :: String -> [(Char, Char, Char)]
+zipS :: [a] -> [(a, a, a)]
 zipS s = zip3 s (tail s) (tail (tail s))
 
 zipThrees :: ([(a, a, a)], [(a, a, a)], [(a, a, a)]) -> [((a, a, a), (a, a, a), (a, a, a))]
@@ -25,4 +24,7 @@ zipThrees (a, b, c) = zip3 a b c
 
 accessable :: ((Char, Char, Char), (Char, Char, Char), (Char, Char, Char)) -> Bool
 accessable (_, (_, '.', _), _) = False
-accessable ((a, b, c), (d, e, f), (g, h, i)) = sum (map (fromEnum . (== '@')) [a, b, c, d, e, f, g, h, i]) < 5
+accessable grid = countAtSigns grid < 5
+  where
+    countAtSigns ((a, b, c), (d, e, f), (g, h, i)) =
+      length $ filter (== '@') [a, b, c, d, e, f, g, h, i]
